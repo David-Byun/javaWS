fb4d0059e4f5ae67ad548a1114f2e2c742178c71
David Byun <byundavid@naver.com>
2023-02-27 16:51:48 +0900

    팀 과제 2번 문항 작성

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,5 +1,7 @@
 package ws1;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
@@ -20,11 +22,37 @@ public class Ws {
 				// 배열의 합과 평균을 출력한다.
 				
 			} else if(cmd.equals("a2")) {
+				System.out.println("5~9까지의 숫자를 입력해주세요");
+
 				// 5~9까지의 숫자를 입력받는다.
+				int input = Integer.parseInt(sc.next());
+
 				// 입력받은 숫자만큼 배열을 생성하고
+				int[] arr = new int[input];
+
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				Random r = new Random();
+
+				for (int i = 0; i < arr.length; i++) {
+					arr[i] = r.nextInt(10);
+				}
+
 				// 배열 정보를 출력한다.
+				System.out.println("배열 정보 출력 : " + Arrays.toString(arr));
+
 				// 짝수번째 숫자들의 합과 평균을 출력한다.
+				int sum = 0;
+				double avg = 0.0;
+				int cnt = 0;
+				for (int i = 0; i < arr.length; i++) {
+					if (i % 2 != 0) {
+						sum += arr[i];
+						cnt++;
+					}
+				}
+
+				System.out.println("짝수번째 숫자들의 합 = " + sum);
+				System.out.println("짝수번째 숫자들의 평균 = " + (double) sum / cnt );
 				
 			} else if(cmd.equals("a3")) {
 				// 5~9까지의 숫자를 입력받는다.
