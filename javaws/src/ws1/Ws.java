453e873ec521f38146d95823fae4e4df1c4d33cd
helloyeojin <bonuibomb@gmail.com>
2023-02-28 09:07:32 +0900

    yeojin commit

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -9,17 +9,30 @@ public class Ws {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(true) {
-			System.out.println("Input Command[q, i, d, s]");
+			System.out.println("Input Command[q, a1, a2, a3, a4]");
 			String cmd = sc.next();
 			if(cmd.equals("q")) {
 				System.out.println("Quit...");
 				break;
-			} else if(cmd.equals("a1")) {
-				// 5~9까지의 숫자를 입력받는다.
-				// 입력받은 숫자만큼 배열을 생성하고
-				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
-				// 배열 정보를 출력한다.
-				// 배열의 합과 평균을 출력한다.
+			} else if(cmd.equals("a1")) {System.out.println("5~9까지의 숫자를 입력해주세요");
+
+			int input = Integer.parseInt(sc.next());
+			int[] arr = new int[input];
+			Random r = new Random();
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = r.nextInt(9)+1;
+			}
+			System.out.println("배열 정보 출력 : " + Arrays.toString(arr));
+		    int sum = 0;
+			int cnt = 0;
+			for (int i = 0; i < arr.length; i++) {
+					sum += arr[i];
+					cnt++;	
+			}
+
+			System.out.println( sum);
+			System.out.println((double) sum / cnt );
+				
 				
 			} else if(cmd.equals("a2")) {
 				System.out.println("5~9까지의 숫자를 입력해주세요");
@@ -38,7 +51,11 @@ public class Ws {
 				}
 
 				// 배열 정보를 출력한다.
+
+				// 배열의 합과 평균을 출력한다.
+
 				System.out.println("배열 정보 출력 : " + Arrays.toString(arr));
+
 
 				// 짝수번째 숫자들의 합과 평균을 출력한다.
 				int sum = 0;
@@ -56,10 +73,10 @@ public class Ws {
 			} else if(cmd.equals("a3")) {
 				
 				// 5~9까지의 숫자를 입력받는다.
-				System.out.println("5~9 사이의 숫자를 하나 입력해주세요");
+				System.out.println("5~9 사이의 숫자를 하나 입력해주세요.");
 				int n3 = Integer.parseInt(sc.next());
 				if (n3 < 5 || n3 > 9) {
-					System.out.println("잘못 입력하셨습니다. 5~9 사이의 숫자를 입력해주세요");
+					System.out.println("잘못 입력하셨습니다. 처음으로 돌아갑니다.");
 				} else {
 					
 				// 입력받은 숫자만큼 배열을 생성하고
@@ -100,7 +117,7 @@ public class Ws {
 				}
 				
 			} else if(cmd.equals("a4")) {
-				// 5~9까지의 수를 2개 입력받는다.
+				// 5~9까지의 수를 2개 입력받는다..
 				// 2차원 배열을 만든다.
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
 				// 배열 정보를 출력한다.
